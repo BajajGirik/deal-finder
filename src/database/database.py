267088cada15908fd94c3d5cb540ee9c99b870ad
Girik Bajaj <bajajgirik2010@gmail.com>
2023-07-17 00:02:44 +0530

    Add load_env method to get DATABASE_URL

--- src/database/database.py
+++ src/database/database.py
@@ -1,6 +1,9 @@
 import os
+from dotenv import load_dotenv
 from pymongo import MongoClient
 from database.productTracker import ProductTrackerService
+
+load_dotenv()
 
 class Database:
     def __init__(self) -> None:
