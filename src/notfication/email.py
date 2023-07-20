52e800da6e9995d6ed7f50cac54af209a6804682
Girik Bajaj <bajajgirik2010@gmail.com>
2023-07-20 18:29:11 +0530

    Rename file

--- src/notfication/email.py
+++ src/notfication/email.py
@@ -10,14 +10,14 @@ class EmailNotification:
     __serviceAccEmail = os.environ.get('SERVICE_EMAIL')
     __serviceAccPassword = os.environ.get('SERVICE_EMAIL_PASSWORD')
     _Default_subject = " Something went wrong !!"
-   
+
     @staticmethod
     def sendEmail(message:str,subject:str,to:list[str]):
         if not EmailNotification.__serviceAccEmail or not EmailNotification.__serviceAccPassword:
             raise Exception("Please provide both service email and password to send email")
 
         context = ssl.create_default_context()
-        with smtplib.SMTP_SSL('smtp.gmail.com', 465 , context=context) as gmail:    
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465 , context=context) as gmail:
             gmail.login(EmailNotification.__serviceAccEmail, EmailNotification.__serviceAccPassword)
             for dest in to:
                 newEmail = EmailMessage()
