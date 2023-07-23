9ee681b161abdaccaad14b0f15b11819e89cdb09
Girik Bajaj <bajajgirik2010@gmail.com>
2023-07-23 22:38:32 +0530

    Best Practices: Rename methods and variables in notification folder

--- deal_finder/notification/email.py
+++ deal_finder/notification/email.py
@@ -7,24 +7,23 @@ import ssl
 load_dotenv()
 
 class EmailNotification:
-    __serviceAccEmail = os.environ.get('SERVICE_EMAIL')
-    __serviceAccPassword = os.environ.get('SERVICE_EMAIL_PASSWORD')
-    _Default_subject = " Something went wrong !!"
+    __service_account_email = os.environ.get('SERVICE_EMAIL')
+    __service_account_password = os.environ.get('SERVICE_EMAIL_PASSWORD')
+    DEFAULT_SUBJECT = " Something went wrong !!"
 
     @staticmethod
-    def sendEmail(message:str,subject:str,to:list[str]):
-        if not EmailNotification.__serviceAccEmail or not EmailNotification.__serviceAccPassword:
+    def send_email(message:str,subject:str,to:list[str]):
+        if not EmailNotification.__service_account_email or not EmailNotification.__service_account_password:
             raise Exception("Please provide both service email and password to send email")
 
         context = ssl.create_default_context()
         with smtplib.SMTP_SSL('smtp.gmail.com', 465 , context=context) as gmail:
-            gmail.login(EmailNotification.__serviceAccEmail, EmailNotification.__serviceAccPassword)
-            for dest in to:
-                newEmail = EmailMessage()
-                newEmail['From']=EmailNotification.__serviceAccEmail
-                newEmail['Subject']= subject if subject else EmailNotification._Default_subject
-                newEmail.set_content(message)
-                newEmail['To']= dest
-                gmail.sendmail(EmailNotification.__serviceAccEmail, dest, newEmail.as_string())
+            gmail.login(EmailNotification.__service_account_email, EmailNotification.__service_account_password)
+            for destination_mail in to:
+                email_message = EmailMessage()
+                email_message['From']=EmailNotification.__service_account_email
+                email_message['Subject']= subject if subject else EmailNotification.DEFAULT_SUBJECT
+                email_message.set_content(message)
+                email_message['To']= destination_mail
+                gmail.sendmail(EmailNotification.__service_account_email, destination_mail, email_message.as_string())
             gmail.quit()
-
