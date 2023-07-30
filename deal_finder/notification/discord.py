54dca22901839a0d7ee3893c6fbcb929ca46c592
Girik Bajaj <bajajgirik2010@gmail.com>
2023-07-30 17:13:32 +0530

    Make client and token private variables

--- deal_finder/notification/discord.py
+++ deal_finder/notification/discord.py
@@ -12,21 +12,21 @@ class DiscordClient(discord.Client):
 
 
 class DiscordClientWrapper:
-    _token = os.getenv('TOKEN')
+    __token = os.getenv('TOKEN')
 
     def __init__(self) -> None:
         intents = discord.Intents.default()
         intents.message_content = True
-        self.client = discord.Client(intents=intents)
-        if DiscordClientWrapper._token == None:
+        self.__client = discord.Client(intents=intents)
+        if DiscordClientWrapper.__token == None:
             print("Token not provided")
             # TODO: Send error notification
         else:
-            self.client.run(token=DiscordClientWrapper._token)
+            self.__client.run(token=DiscordClientWrapper.__token)
 
     async def send_message_to_channel(self, channel_id: int, message_text: str) -> Optional[int]:
         try:
-            channel = self.client.get_channel(channel_id)
+            channel = self.__client.get_channel(channel_id)
             if channel != None:
                 message = await channel.send(message_text)
                 return message.id
