fb789aff765d2318437b93ae08bd57f9175d4d96
Girik Bajaj <bajajgirik2010@gmail.com>
2023-08-05 18:54:48 +0530

    Add logs for products_tracker job

--- deal_finder/jobs/products_tracker.py
+++ deal_finder/jobs/products_tracker.py
@@ -1,3 +1,4 @@
+from time import time
 from typing import List, Optional, TypedDict
 from database.database import Database
 from database.product_tracker import ProductTrackerModel
@@ -15,7 +16,11 @@ class ProductTrackingResult(TypedDict):
 class ProductsTrackerJob:
     @staticmethod
     def execute() -> List[ProductTrackingResult]:
+        start_time = time()
+
         products = database.product_tracker.find_all()
+        print(f"Tracking total {len(products)} product(s)")
+
         tracked_product_results: List[ProductTrackingResult] = []
 
         for product in products:
@@ -23,11 +28,16 @@ class ProductsTrackerJob:
             if result:
                 tracked_product_results.append(result)
 
+        print(f"Found optimial prices for {len(tracked_product_results)} product(s)")
+
+        total_time = round(time() - start_time, 2)
+        print(f"Total time taken to track {len(products)} product(s): {total_time} seconds")
         return tracked_product_results
 
 
     @staticmethod
     def track_product(product: ProductTrackerModel) -> Optional[ProductTrackingResult]:
+        print(f"Tracking product {product['_id']}", end=": ")
         min_price = None
         min_price_available_on = None
 
@@ -44,6 +54,8 @@ class ProductsTrackerJob:
                 min_price_available_on = url
 
         if not min_price or not min_price_available_on:
+            print("Unable to find optimal price")
             return None
 
+        print("Found optimal price")
         return ProductTrackingResult(lowest_price=min_price, available_on=min_price_available_on, channel_id_to_notify=product["channel_id"])
