4f43c6cb5bec923a6f3e4a379fad65b4db867f9c
Girik Bajaj <bajajgirik2010@gmail.com>
2023-08-15 20:47:03 +0530

    Handle missing env for database class

--- deal_finder/database/database.py
+++ deal_finder/database/database.py
@@ -2,6 +2,8 @@ import os
 from dotenv import load_dotenv
 from pymongo import MongoClient
 from database.product_tracker import ProductTrackerService
+from constants import ERROR_NOTIFICATION_EMAIL_ADDRESSES
+from notification.notification import Notification
 
 load_dotenv()
 
@@ -11,10 +13,22 @@ class Database:
        database_name = os.getenv('DATABASE_NAME')
 
        if not database_url or not database_name:
-           # TODO: Notify via email
-           print("DATABASE_URL or DATABASE_NAME not provided")
+           notification = Notification()
+
+           message = "Missing environment variable(s): "
+           if not database_url and not database_name:
+               message += "DATABASE_URL and DATABASE_NAME\n"
+           elif not database_url:
+               message += "DATABASE_URL\n"
+           else:
+               message += "DATABASE_NAME\n"
+
+           message += "Please check your .env file to resolve this error."
+
+           notification.email.send_email(message, "Missing ENV", ERROR_NOTIFICATION_EMAIL_ADDRESSES)
+           raise Exception("Missing ENV")
 
        self.__client = MongoClient(database_url)
-       self.__database = self.__client[database_name or "deals"]
+       self.__database = self.__client[database_name]
 
        self.product_tracker = ProductTrackerService(self.__database['product_tracker'])
