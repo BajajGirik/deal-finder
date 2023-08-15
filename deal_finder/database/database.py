8e1681c89842aa4c1161e08d0d633e226be6ec2c
Girik Bajaj <bajajgirik2010@gmail.com>
2023-08-15 20:28:34 +0530

    Add database name to .env

--- deal_finder/database/database.py
+++ deal_finder/database/database.py
@@ -8,12 +8,13 @@ load_dotenv()
 class Database:
     def __init__(self) -> None:
        database_url = os.getenv('DATABASE_URL')
+       database_name = os.getenv('DATABASE_NAME')
 
-       if not database_url:
+       if not database_url or not database_name:
            # TODO: Notify via email
-           print("DATABASE_URL not provided")
+           print("DATABASE_URL or DATABASE_NAME not provided")
 
        self.__client = MongoClient(database_url)
-       self.__database = self.__client['deals']
+       self.__database = self.__client[database_name or "deals"]
 
        self.product_tracker = ProductTrackerService(self.__database['product_tracker'])
